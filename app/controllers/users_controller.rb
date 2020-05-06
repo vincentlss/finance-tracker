c162d123608872f905fcf67febf8105b6ff9865a
vincentlss <shyanshinvincent@gmail.com>
2020-05-06 22:20:58 +0800

    Allow adding and removal of friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends.present?
+        respond_to do |format|
+          format.js {render partial: 'users/friend_result'}
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid friend name or email"
+          format.js {render partial: 'users/friend_result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email"
+        format.js {render partial: 'users/friend_result'}
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
+
 end
